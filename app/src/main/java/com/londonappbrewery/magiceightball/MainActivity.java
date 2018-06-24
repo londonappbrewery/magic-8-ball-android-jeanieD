5e96d02a86afbcfb9ac7ab58d55a4f95e74d10e5
Jeanie_D <jeaniemd@hotmail.com>
2018-06-24 16:44:05 -0700

    Tutorial project complete! Ask the Magic Eightball!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,15 @@
 package com.londonappbrewery.magiceightball;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.lang.reflect.Array;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView ballDisplay = (ImageView) findViewById(R.id.image_eightBall);
+        /* create the array variable for the ball */
+        final int[] ballArray = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        Button askButton;
+        askButton = (Button) findViewById(R.id.askButton);
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Eightball", "button pressed!");
+
+                //create a random number generator
+                Random randomNumberGenerator = new Random();
+                //assign the random number to 'number' variable
+                int number = randomNumberGenerator.nextInt(5);
+                Log.d("Eightball", "the number is" + number);
+
+                //use the random number to pull the index of the image array
+                ballDisplay.setImageResource(ballArray[number]);
+
+
+            }
+        });
+
+
     }
 }
